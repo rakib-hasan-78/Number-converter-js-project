272e066a48e35228779c529a53adcf5e3bc73161
rakib-hasan-78 <rakib.webdev78@gmail.com>
2024-02-10 07:26:45 +0600

    last

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ document.getElementById('root').innerHTML=`
         <main>
             <div class="container">
                 <div class="content mt-5">
-                    <h1 id="project-title" class="text-center text-uppercase fw-bolder">formatting number to shorter version.</h1>
+                    <h1 id="project-title" class="text-center text-uppercase fw-bolder card-title">formatting number to shorter version.</h1>
                     <div class="row gy-3 gx-3 p-5 justify-content-center align-items-center">
                         <div class="col-xl-5">
-                            <div class="card p-2 pb-4">
+                            <div class="card glass-card p-2 pb-4">
                                 <div class="card-body">
-                                <h3 class="card-title text-center text-capitalize fs-4 fw-bold">covert any number</h3>
+                                <h3 class="card-title text-center text-capitalize fs-4 fw-bold card-title">covert any number</h3>
                                 </div>
                                 <div class="cardE-element">
-                                    <input id="number-input" type="text" class="form-control form-control-sm fw-bold fs-5 text-center text-capitalize" placeholder="provide your number to convert"/>
+                                    <input id="number-input" type="text" class="form-control form-control-sm fw-bold fs-5 text-center text-capitalize input-background" placeholder="provide your number to convert"/>
                                     <button id="btn" class="btn btn-success d-flex mx-auto mt-3 text-capitalize fs-4 fw-bolder"> convert the number </button>
                                 </div>
                             </div> 
@@ -29,14 +29,14 @@ document.getElementById('root').innerHTML=`
         <div class="container">
             <div class="row gy-3 gx-3 justify-content-center align-items-center">
                 <div class="col-xl-5">
-                    <div id="card" class="card d-none p-2">
+                    <div id="card" class="card glass-card d-none p-2">
                         <div class="card-body d-flex justify-content-end align-items-center fs-4 ">
                          <span id="x-icon" class="pointer"><i class="fa-solid fa-x"></i></span>
                         </div>
                         <div class="cardE-element">
-                            <h3 id="resultTitle" class="d-none text-center text-uppercase align-items-center justify-content-center"></h3>
+                            <h3 id="resultTitle" class="d-none text-center text-uppercase align-items-center justify-content-center fw-bolder"></h3>
                             <div id="result-circle" class="result-cercel p-3 border d-none justify-content-center align-items-center mx-auto shadow-lg">
-                                <h3 id="result-counter" class="d-none"></h3>
+                                <h3 id="result-counter" class="d-none result-title"></h3>
                             </div>
                             <p id="result-text" class="d-none justify-content-center lead text-center text-capitalize fs-4"></p>
                         </div>
@@ -47,13 +47,14 @@ document.getElementById('root').innerHTML=`
     </section>
     <footer class="mt-5 w-100">
         <main>
-        <div class="container">
-            <div class="footer-content">
-                <p class="lead text-center text-capitalize">&copy; ${new Date().getFullYear()} all the rights reserved.</p>
+        <div class="container pt-5">
+            <div class="footer-content pt-5">
+                <p class="lead text-center text-capitalize m-0 p-0 pt-5 card-title">&copy; ${new Date().getFullYear()} all the rights reserved.</p>
             </div>
         </div>
         </main>
     </footer>
+
 `
 const numberInput = document.getElementById('number-input');
 const card = document.getElementById('card');
--- style.js
+++ style.js
@@ -5,6 +5,26 @@ export const styleFunction =()=>{
         :root{
             --fw-Blod:600;
             --cs-wait: pointer;
+            --cs-green-background: linear-gradient(120deg, #65dd9e, #e3e9b1);
+            --card-title-color: #333;
+            --input-background : rgb(38 169 38 / 60%);
+            --cs-placeholder-white: rgb(221 241 221 / 60%);
+            --cs-placeholder-Text-white:rgb(231 235 231 / 60%);
+            --cs-white:rgb(122 163 95);
+            --global-white: rgb(245,245,245);
+        }
+        *{
+            margin:0;
+            padding:0;
+            box-sizing: border-box;
+        }
+        html{
+            min-height:100%;
+        }
+        body{
+            width:100%;
+            height:100%;
+            background:var(--cs-green-background) !important;
         }
         #x-icon .fa-x {
             transition: 150ms all ease-in-out;
@@ -26,9 +46,39 @@ export const styleFunction =()=>{
         .not-allowed{
             cursor: var(--cs-wait) !important;
         }
+        .glass-card {
+            background-color: rgba(144, 238, 144, 0.6);
+            backdrop-filter: blur(10px);
+            border-radius: 20px;
+            box-shadow: 0 8px 32px rgba(31, 38, 135, 0.37);
+            padding: 20px;
+        }
+        .card-title{
+            color:var(--cs-white);
+        }
+        .input-background{
+            background:var(--input-background);
+            border:none;
+            outline:none;
+        }
+        .input-background:focus{
+            background:var(--input-background);
+            border:none;
+            outline:none !important;
+            box-shadow:none;
+            color: var(--cs-placeholder-white) !important;
+        }
+        .input-background::placeholder{
+            color: var(--cs-placeholder-Text-white) !important;
+        }
+        
+        .result-title{
+            color:var(--global-white);
+        }
     `
     const styleElement = document.createElement('style');
     styleElement.innerHTML = style;
     document.head.appendChild(styleElement);
 
 };
+
